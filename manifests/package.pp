ce4a42229282daae76b7ad333522966b34ba480f
Frank Wall <fw@moov.de>
2018-01-27 23:59:45 +0100

    add FreeBSD support, style and documentation fixes

--- manifests/package.pp
+++ manifests/package.pp
@@ -7,13 +7,15 @@
 # -------
 #
 # Marius Rieder <marius.rieder@durchmesser.ch>
+# Bernhard Frauendienst <puppet@nospam.obeliks.de>
 #
 # Copyright
 # ---------
 #
 # Copyright 2016 Marius Rieder <marius.rieder@durchmesser.ch>
+# Copyright 2017 Bernhard Frauendienst <puppet@nospam.obeliks.de>
 #
-class postfix::package {
+class postfix::package inherits postfix {
   package { 'postfix':
     ensure => $::postfix::package_ensure,
     name   => $::postfix::package_name,
