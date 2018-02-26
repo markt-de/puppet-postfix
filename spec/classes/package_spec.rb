813585eb50c252efff6e355685b0ae8a541d0879
Bernhard Frauendienst <puppet@nospam.obeliks.de>
2018-02-27 00:02:47 +0100

    rubocop: fix context names

--- spec/classes/package_spec.rb
+++ spec/classes/package_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
 describe 'postfix' do
-  context 'postfix::package' do
+  context 'when including postfix::package' do
     describe 'with default values for all parameters' do
       it { is_expected.to contain_package('postfix') }
       it { is_expected.to contain_package('mailx') }
     end
 
-    context 'w/o mailx management' do
+    context 'without mailx management' do
       let(:params) { { mailx_manage: false } }
 
       it { is_expected.not_to contain_package('mailx') }
--- spec/classes/service_spec.rb
+++ spec/classes/service_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 describe 'postfix' do
-  context 'postfix::service' do
+  context 'when including postfix::service' do
     describe 'with default values for all parameters' do
       it { is_expected.to contain_service('postfix') }
     end
 
-    context 'w/o service management' do
+    context 'without service management' do
       let(:params) { { service_manage: false } }
 
       it { is_expected.not_to contain_service('postfix') }
