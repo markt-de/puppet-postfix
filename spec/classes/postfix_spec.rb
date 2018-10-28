615b98e57890137ed7188d00b1e31a397f0b7506
Bernhard Frauendienst <puppet@nospam.obeliks.de>
2018-10-28 15:40:48 +0100

    Notify service on config changes
    The config package did not have a notify-relationship to the service
    package. This patch fixes this, causing postfix to get reloaded and
    picking up the new config upon changes.
    
    Patch provided by Martijn Heemels (see #8).
    
    Fixes #7.

--- spec/classes/postfix_spec.rb
+++ spec/classes/postfix_spec.rb
@@ -8,6 +8,10 @@ describe 'postfix' do
 
     it { is_expected.to contain_class('postfix::package') }
 
-    it { is_expected.to contain_class('postfix::service').that_requires('Class[postfix::package]') }
+    it { is_expected.to contain_class('postfix::config').that_requires('Class[postfix::package]') }
+
+    it { is_expected.to contain_class('postfix::config').that_notifies('Class[postfix::service]') }
+
+    it { is_expected.to contain_class('postfix::service').that_requires('Class[postfix::config]') }
   end
 end
