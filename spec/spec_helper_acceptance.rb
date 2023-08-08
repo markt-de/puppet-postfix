d965349196b26dd2e4f934979fa02a440a8f7093
Frank Wall <fw@moov.de>
2023-08-08 22:41:24 +0200

    setup Litmus + GitHub Actions

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,22 +1,6 @@
-require 'beaker-rspec'
+# frozen_string_literal: true
 
-# Install Puppet on all hosts
-install_puppet_on(hosts)
+require 'puppet_litmus'
+require 'spec_helper_acceptance_local' if File.file?(File.join(File.dirname(__FILE__), 'spec_helper_acceptance_local.rb'))
 
-RSpec.configure do |c|
-  module_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-
-  c.formatter = :documentation
-
-  c.before :suite do
-    # Install module to all hosts
-    hosts.each do |host|
-      install_dev_puppet_module_on(host, source: module_root, module_name: 'postfix',
-                                         target_module_path: '/etc/puppet/modules')
-      # Install dependencies
-      on(host, puppet('module', 'install', 'puppetlabs-stdlib'))
-
-      # Add more setup code as needed
-    end
-  end
-end
+PuppetLitmus.configure!
